6cc8e80deb269a52d901b34bbf2e3428e0a6c51f
Armando Bustos <armanjbustos596@gmail.com>
2016-10-06 09:28:05 -0600

    reports

---
+++ users.sql
@@ -0,0 +1,31 @@
+USE `seguimientofzt`;
+INSERT INTO `users` (`name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) 
+VALUES 
+('Gabriela Alvarez','gaalvarez@fundacionzt.org','$2y$10$fV0M2oOJecVQ.kNo/bPIgen1OTqgdgV7NyOOr1CxSxfH4x5bjs9Ha','xmSUbhDsFUJBnkDbp9UU0ptnNdaeJ8bA0VDrFUFPO0RrcEmLmFTSeTFtXxf6','2016-06-20 18:27:40','2016-10-04 13:03:25'),
+
+('Anielka Oviedo','aloviedo@fundacionzt.org ','$2y$10$G1j3r/LVVSsv95xZDqxis.aLy389dm0YBLTK3hJYBwYxSHMahfeHm','dpJXMJKaybDK8ksGdXLGmIybZN4yEi4i3fpksVRWUQ1rzIRfRv7gGGUjCnuT','2016-07-11 14:15:28','2016-10-04 15:56:58'),
+('Roxana Rodríguez','rarodriguez@fundacionzt.org ','$2y$10$Rsoc5oOftC3u8qvKbYAhDuZSfp/ocec5lVw251oisu.oTeIp86Xdu','Rxvh4v3r9kpHb9Q9zrXqaqgHMsL4SI3BdOFy252arQxaEyt5OX2kDv11JX7a','2016-07-11 14:21:47','2016-09-01 17:30:12'),
+('Magda Aguirre','maguirref@fundacionzt.org','$2y$10$yH6.IgfnGU1qRwTPrWuxN.BC7grZNt2nlmxrIP2A5FMkgd4gz8HLG','rIuLrdUnjL1GOTsHVTK4m8JqqWvvPv1itrYRWJJyQf7lnMPfJPpvTl9mieCO','2016-07-12 13:09:36','2016-07-12 13:11:45'),
+('Dayton Calderón','djcalderon@fundacionzt.org ','$2y$10$juQVVQsPCaYuDt/hW4WZNeTF/yrrWhqE/LLoG5OoK5wbWE4rruCKu','JfeJiyin7qSw7wbe0NC7TXG0dxsTZyzxBJUkzLbDuQp7XtEx6vCpUDLZcXRv','2016-07-12 13:14:53','2016-07-12 13:15:26'),
+('Jairo González','jjgonzalez@fundacionzt.org ','$2y$10$jHAKJRY2UbHtPr3RozugBOnpjl8QuBNlXgUzURI8Tjnq6spzH04nC','SjKs0emRjflH9LtLvTJ0Zf3EZmrVPaV3EXXU64I0N6ES99gZLTfrZtbX5mBv','2016-07-12 13:17:24','2016-07-12 13:17:48'),
+('José Alberto Bosque','jbosque@fztedu.org ','$2y$10$ZX3YPdJknVpLqd.JJPy5Y.wqXDWeUYG.Y2fzW7pYEXvFalmsap4k.','Q3sBJYH43nuYxlQuocVmVSJs7xWMYqCzINIV1kTDFqQluZbuyaFclRfljOkZ','2016-07-12 13:18:55','2016-07-18 13:03:23'),
+('Karen Martínez','kymartinez@fundacionzt.org ','$2y$10$wU/L4Tp93z9MOYTdjnJBWOSEEZti/R0n2W8mjOQ2anQrXVFHVPWgm','c1C9Nu89RmwqDQ6gBpfpRiPQRTSbB0DkKm7IhhCnIKec965VRPxKxkjWrjWT','2016-07-12 13:20:37','2016-08-05 00:40:57'),
+('Kennedy Simmons','ksimmons@fztedu.org ','$2y$10$iEvDAl0gvJlkfPqDOZ4ZPuLNC6me4vbuj2EEdhJ.9xwAa5oQrM73q','NGzNnf5TiZM4DLEeXdz8Y0ZWQoNz5onjp3xgBr2rAaMFEin4GoaWK4lbn9XK','2016-07-12 13:21:36','2016-07-21 20:15:06'),
+('Linton Ugarte','wlrigby@fundacionzt.org ','$2y$10$23YX1WxrTrAjnXcy88Uut.TK.Q04cNgHSQAWxeD2dXpGjwsC7ImNG','nl3mCtT8tDOBJkoLiZlCLzbUqlLjsloOHtlXmMkMpwRHMx6Eq9YFGHYt7XHN','2016-07-12 13:23:17','2016-07-12 13:23:34'),
+('Marlen Flores','mflores@fztedu.org ','$2y$10$tuv0gzMQrD7WixpB1TxpAeJqlXuyPEcAHZm82me5LWwkmhao.chau','ThyyjC3GWmtEYTL0Rwu5CNhxJunKQH6NxLvQccBRJWE0BE8QymkN0qpdGqMh','2016-07-12 13:24:41','2016-07-16 12:30:35'),
+('Seneyda Garcí­a','segarcia@fundacionzt.org ','$2y$10$hrLRD6AI.3biHjp7vjZJ..FQsTDY5I6Oo4av3HvWm0p/RkY8euqXK','ANYCQ5qg0OrzHlafMFRKgxNJZfwh8Wb2RAQr8sPzlpNgvQ9Pzz02OOhHLAIn','2016-07-12 13:25:46','2016-07-12 13:26:27'),
+('Bianora Alvarez','bialvarez@fundacionzt.org ','$2y$10$2d83xpo3tsSo3xIJIFYVbuRkTqnrz1BhblMZJJzkZX1NEzJD6HFZW','7lzgh5AHswEFAhnWjMW7sLgX1atoSxnTr3fKin93JgpVqqYTakl1nI2WAifv','2016-07-12 13:27:31','2016-07-12 13:28:02'),
+('Claudia Moreno','cmoreno@fztedu.org','$2y$10$CGbLfRACeAVAUHS4CtTrAeq8eiD30aa88uqgLlkB/VMRITdI79Ory','KlIGgpGpWH1U1G9EshoPH5mrw0TPAadvGt7F0Kx8KoEySOz1ZfWt7rEst8dp','2016-07-12 13:28:30','2016-09-23 13:18:08'),
+('Ezer Calderón','ecalderon@fundacionzt.org ','$2y$10$cCqcs90MveNwBIn4ceG1wuMOMX2nWi2HdJSxtzB8WbVqtFH7Pl9NC','JfAcRLpPVsqs2zpCOCaRPOKXvX46hJeuJcFfd27vuQJA66dc9qDchr87dOln','2016-07-12 13:29:48','2016-08-12 18:24:43'),
+('Frannia Araquistain','faraquistain@fundacionzt.org','$2y$10$mKrhYwIBnlh4aAaoa6WPt.Z3sutvzDw1Wq1uXn9e06qSfnsGHvzHm','jgWSUr7apVNFjaOIrhTKTZZbHf2ebAH28SEvuUjw9aseWZ55ymTYm5QMKrFQ','2016-07-12 13:31:16','2016-08-23 15:11:27'),
+('Heriberto Méndez','jmdiaz@fundacionzt.org','$2y$10$XQ.OpiEjYptly1rIy7ma/e06AOROKPfq2oLXTyTHz9r7wmDw0GmTO','3EOBGU8Nzvh9rTQ9W3fQHjGbF8YcBUVF4M49BMyt5AbvVP1ONB6j5K2j4MnX','2016-07-12 13:32:11','2016-09-29 19:35:14'),
+('Junieth Bello','jbsalamanca@fundacionzt.org ','$2y$10$njy3iSDT9rZlNGo3.WwsGemDYl.O3CLC.sDu2QROreBRdOH41uj36','WPv4nBBANaXosz2N0csPmZTSBxoQ9liBeyYZTcZMrYa9AMnoHvo1xLxXPoXr','2016-07-12 13:33:09','2016-07-12 13:33:30'),
+('Luz Amanda Benavides','lbenavides@fundacionzt.org','$2y$10$sFrhu2IWkeckAL/YF0N5ley1DelHeWlzYCYbLX1wWxm/Er0RgKEfa','SUjtZv9Jp88YSd9TiBzGSG1X6LOHXnPV1HCa4bPhQXfbWfeQu1PajhqGsS59','2016-07-12 13:34:01','2016-09-24 12:19:34'),
+('Marí­a Magdalena Hernández','mmhernandez@fundacionzt.org','$2y$10$56MCklHAKBbWi0C01Fr12uoOGpOBRYHbZ23WRu9CD1iuOChLDm2bi','vjXIGw8En9P4EHSgckg9ZF5JyAQaZ6zkfOvK8CfLXFDgDSv4g4NU6neGtvbt','2016-07-12 13:35:17','2016-07-12 13:35:37'),
+('Juan Otoniel Saldaña','jsaldana@fundacionzt.org','$2y$10$4bWHgphR0s25DZCPcKijpuw61tPEZmIt0IdlfK20Ls3D/brecwX32','Je5a7815zsFIYzxCySDJAA90tZJuVPVnYQJQBNd7NwfIM0O74Ze3r2Ic3oYc','2016-07-12 13:36:37','2016-07-12 13:37:03'),
+('Nayiris Urbina','nurbina@fundacionzt.org ','$2y$10$8YzhVCDG/VMhzGzyvl8fjeBdmFZlyM4EaDwc2xeh43BEsBCS.RCQi','uZMY51ZxIpaRAyZDvav6IJ656o1g9wthIdvSpB8tdFYGbZOUXBchZLCYv1ae','2016-07-12 13:37:35','2016-07-16 12:23:29'),
+('Yania López','ylopeza@fundacionzt.org','$2y$10$.P8cdGLaDbbHfPjCPGPQHOthQ9ZPR2U29Jy9LhNiMva0SfUZI3vvO','AD4l8kHhr3CSCkJkJ5Ziap0gTuZnoS5NQJFbPughrpjJvWFODNbxgADquHb3','2016-07-12 13:40:56','2016-09-30 19:24:11'),
+('José López','jlopezg@fundacionzt.org','$2y$10$z/P/F6nrv0luBhJFEAkTReXS4A8eBipMfS5Qo8BJpIQ8gUmpXeRJG','yzbxG0bC2f7Wq2xKoubw4aAzBQu7cFNGwjosY4YZhqPk5YMSscNWhEODizWl','2016-07-16 12:59:02','2016-09-13 15:05:32'),
+('Luis Gutiérrez','lgmolina@fundacionzt.org','$2y$10$sO4.4GQ388Rbs0QeS5JORu1VJTGMOBaApacE0btrsvVj7KmuNEwIC','jROWws8bvT1e7u79Mqmo22ltxoPfWtL7Am5XXeeqImaeJwhBLUpWrFdELNqm','2016-07-29 13:33:33','2016-08-05 21:54:00');
+
+-- Registros anteriores de visitas
